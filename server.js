ecdd3cc8bfad2556b84786691c33f62f7fe7220a
GriffinMartin <gmart248@gmail.com>
2017-04-11 16:44:12 -0400

    requested changes

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ var app = express()
 var port = process.env.PORT || 8080
 
 const pages = [
-	{url: '/ef',name: 'Electric Forest', img: '/img/ef.jpg', link: 'https://www.electricforestfestival.com/'},
-	{url: '/sb',name: 'Sonic Bloom', img: '/img/sb.jpg', link: 'http://sonicbloomfestival.com/'},
-	{url: '/un',name: 'UpNorth', img: '/img/un.jpg', link: 'https://www.upnorthfest.com/'}
+{url: '/ef', name: 'Electric Forest', img: '/img/ef.jpg', link: 'https://www.electricforestfestival.com/'},
+{url: '/sb', name: 'Sonic Bloom', img: '/img/sb.jpg', link: 'http://sonicbloomfestival.com/'},
+{url: '/un', name: 'UpNorth', img: '/img/un.jpg', link: 'https://www.upnorthfest.com/'}
 ]
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
-
 
 app.get('/', function (request, response) {
   response.render('pages/index', {
@@ -19,21 +18,11 @@ app.get('/', function (request, response) {
   })
 })
 
-app.get('/ef', function (request, response) {
-  response.render('pages/ef', {
-    pages: pages
-  })
-})
-
-app.get('/sb', function (request, response) {
-  response.render('pages/sb', {
-    pages: pages
-  })
-})
-
-app.get('/un', function (request, response) {
-  response.render('pages/un', {
-    pages: pages
+pages.forEach(function (page) {
+  app.get(page.url, function (request, response) {
+    response.render('pages/festivals.ejs', {
+      page: page
+    })
   })
 })
 
